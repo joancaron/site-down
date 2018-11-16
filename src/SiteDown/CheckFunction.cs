4f46e4631fe169f3d509c1c9dbf3b5f05528dca5
Joan Caron <caronjoan@gmail.com>
2018-11-16 22:45:58 +0100

    Add sitedown projects

---
+++ src/SiteDown/CheckFunction.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace SiteDown
+{
+    public static class CheckFunction
+    {
+        [FunctionName("Check")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]
+            HttpRequest req,
+            string url,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkResult();
+        }
+    }
+}
