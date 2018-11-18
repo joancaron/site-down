a873aa9bd92a0c5a8e91d0eafb8ebd9a7b44b1a6
Joan Caron <caronjoan@gmail.com>
2018-11-18 17:42:17 +0100

    Create IpRatLimiter

---
+++ src/SiteDown/IpRateLimiter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+using EnsureThat;
+
+using JetBrains.Annotations;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace SiteDown
+{
+    public static class IpRateLimiter
+    {
+        private const string CloudflareIpHeaderKey = "CF-Connecting-IP";
+
+        private const string XForwadedForHeaderKey = "X-Forwarded-For";
+
+        /// <summary>
+        /// Determine if we can execute asynchronously the action.
+        /// </summary>
+        /// <param name="request">The current request. This cannot be null.</param>
+        /// <returns>
+        /// True if we can execute the action, false if not.
+        /// </returns>
+        public static bool CanExecute([NotNull] HttpRequest request, [NotNull] IMemoryCache memoryCache)
+        {
+            EnsureArg.IsNotNull(request);
+
+            string ip;
+
+            if (request.Headers.TryGetValue(CloudflareIpHeaderKey, out var ips)
+                || request.Headers.TryGetValue(XForwadedForHeaderKey, out ips))
+            {
+                ip = ips.First();
+            }
+            else
+            {
+                ip = request.HttpContext.Connection.RemoteIpAddress.ToString();
+            }
+
+            var canExecute = !memoryCache.TryGetValue(ip, out _);
+
+            if (canExecute)
+            {
+                using (var entry = memoryCache.CreateEntry(ip))
+                {
+                    entry.SlidingExpiration = TimeSpan.FromMilliseconds(500);
+                }
+            }
+
+            return canExecute;
+        }
+    }
+}
---
+++ test/SiteDown.Tests/IpRateLimiterTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Primitives;
+
+using Moq;
+
+using Xunit;
+
+namespace SiteDown.Tests
+{
+    public class IpRateLimiterTests
+    {
+        private readonly TimeSpan _defaultCacheDuration = TimeSpan.FromMilliseconds(10);
+
+        private readonly Mock<HttpRequest> _requestMock;
+
+        private readonly MemoryCache _memoryCache;
+
+        public IpRateLimiterTests()
+        {
+            var ip = new StringValues("1.1.1.1");
+            _requestMock = new Mock<HttpRequest>();
+            _requestMock.Setup(request => request.Headers.TryGetValue(It.IsAny<string>(), out ip)).Returns(true);
+            _memoryCache = new MemoryCache(new MemoryCacheOptions());
+        }
+
+        [Fact]
+        public void ShouldBlockRegisteredIps()
+        {
+            IpRateLimiter.CanExecute(_requestMock.Object, _memoryCache);
+            var canExecute = IpRateLimiter.CanExecute(_requestMock.Object, _memoryCache);
+            Assert.False(canExecute);
+        }
+
+        [Fact]
+        public void ShouldNotBlockNewIps()
+        {
+            Assert.True(IpRateLimiter.CanExecute(_requestMock.Object, _memoryCache));
+        }
+
+        [Fact]
+        public void ShouldNotBlockRegisteredIpsAfterCacheExpiration()
+        {
+            IpRateLimiter.CanExecute(_requestMock.Object, _memoryCache);
+            Thread.Sleep(_defaultCacheDuration);
+            Assert.True(IpRateLimiter.CanExecute(_requestMock.Object, _memoryCache));
+        }
+
+        [Fact]
+        public void ShouldTakeRemoteIpWhenNoHeader()
+        {
+            var ip = new StringValues("1.1.1.1");
+            var expectedIp = IPAddress.Parse("2.2.2.2");
+
+            _requestMock.Setup(request => request.Headers.TryGetValue(It.IsAny<string>(), out ip)).Returns(false);
+            _requestMock.Setup(request => request.HttpContext.Connection.RemoteIpAddress).Returns(expectedIp);
+
+            var canExecute = IpRateLimiter.CanExecute(_requestMock.Object, _memoryCache);
+
+            Assert.True(canExecute);
+            Assert.True(_memoryCache.TryGetValue(expectedIp.ToString(), out _));
+        }
+    }
+}
